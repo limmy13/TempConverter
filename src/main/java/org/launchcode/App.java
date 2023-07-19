99bb016fca731a1633e325432f16603d45383536
Jasmine Lim <jasmine_lim13@yahoo.com>
2023-07-19 18:30:28 -0500

    in class coding

--- src/main/java/org/launchcode/App.java
+++ src/main/java/org/launchcode/App.java
@@ -1,7 +1,17 @@
 package org.launchcode;
 
 public class App {
-    public static void main(String[] args) {
-        double tempC = Converter.convert(212);
+    public static void main(String[] args)  { // if you put this: throws NegativeValueException here, it's a note on the method and
+        // problem will continue unless handled
+        try {
+            double tempC = Converter.convert(-212);
+//          System.out.println(tempC); //debugging
+            //throwing means somewhere else has to deal with it
+            //catch means you are going to handle it
+        } catch (NegativeValueException ex) {
+            System.out.println("No negative temperatures.");
+//            System.exit(100); // non-zero means error and 0 means success
+        }
+        System.out.println("Completed successfully!");
     }
 }
--- src/main/java/org/launchcode/Converter.java
+++ src/main/java/org/launchcode/Converter.java
@@ -1,7 +1,11 @@
 package org.launchcode;
 
 public class Converter {
-    public static double convert(double tempF) {
+    public static double convert(double tempF) throws NegativeValueException {
+        if (tempF < 0) {
+            throw new NegativeValueException(); // either catch the exception or declare the method that throws it
+
+        }
         return tempF - 32 * 9 / 5;
     }
 }
