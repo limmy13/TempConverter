39c2655c0b3e2b662802c69243f2087de926a431
Tom Lynch <lync0143@gmail.com>
2023-07-19 17:14:04 -0500

    Initial commit.

---
+++ src/main/java/org/launchcode/App.java
@@ -0,0 +1,7 @@
+package org.launchcode;
+
+public class App {
+    public static void main(String[] args) {
+        double tempC = Converter.convert(212);
+    }
+}
---
+++ src/main/java/org/launchcode/Converter.java
@@ -0,0 +1,7 @@
+package org.launchcode;
+
+public class Converter {
+    public static double convert(double tempF) {
+        return tempF - 32 * 9 / 5;
+    }
+}
